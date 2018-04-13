769ca71d8cdfb208b6c7dbd328d95b0de460316a
PeterMcCormick <mcpeter@udel.edu>
2018-04-13 09:31:57 -0400

    Some more progress into service

--- src/main/java/io/zipcoder/service/WithdrawalService.java
+++ src/main/java/io/zipcoder/service/WithdrawalService.java
@@ -1,5 +1,22 @@
 package io.zipcoder.service;
 
+import io.zipcoder.domain.Withdrawal;
+import io.zipcoder.repository.WithdrawalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WithdrawalService {
 
+
+    WithdrawalRepository withdrawalRepository;
+
+    @Autowired
+    public WithdrawalService(WithdrawalRepository withdrawalRepository) {
+        this.withdrawalRepository = withdrawalRepository;
+    }
+
+    public Withdrawal getWithdrawalByWithdrawalId(Long id) {
+        this
+    }
 }
