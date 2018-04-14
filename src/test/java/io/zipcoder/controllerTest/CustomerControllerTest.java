aa238effeb2aff24bbb41b0d264f2e7465018012
Carolynn Vansant <cvansant@MacBook-Pro-7.local>
2018-04-14 15:26:53 -0400

    started adding mockito testing

---
+++ src/test/java/io/zipcoder/controllerTest/CustomerControllerTest.java
@@ -0,0 +1,41 @@
+package io.zipcoder.controllerTest;
+
+import io.zipcoder.controller.CustomerController;
+
+import io.zipcoder.service.CustomerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.*;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Before
+    public void setup(){
+
+    }
+}
